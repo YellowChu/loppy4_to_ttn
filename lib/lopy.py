81650f15eecf16dfe9e581da3c16aec19f42839f
YellowChu <nguyen.david1996@gmail.com>
2021-04-01 21:57:52 +0200

    gathers secrets from nvram now

--- lib/lopy.py
+++ lib/lopy.py
@@ -12,8 +12,9 @@ def connect():
     if not lora.has_joined():
         print("Fristly join")
         # create an OTAA authentication parameters, change them to the provided credentials
-        app_eui = ubinascii.unhexlify('0000000000000000')
-        app_key = ubinascii.unhexlify('D20FAEC47B8610BFA234A62402240EE4')
+
+        app_eui = ubinascii.unhexlify(pycom.nvs_get("app_eui"))
+        app_key = ubinascii.unhexlify(pycom.nvs_get("app_key"))
         # join a network using OTAA (Over The Air Activation)
         lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
 
@@ -26,13 +27,6 @@ def connect():
             time.sleep(1)
     else:
         print("Connection restored")
-
-    # # create an ABP authentication parameters
-    # dev_addr = struct.unpack(">l", ubinascii.unhexlify('26013660'))[0]
-    # nwk_swkey = ubinascii.unhexlify('3AF9FB99FD7576CE48441A6673D49ED0')
-    # app_swkey = ubinascii.unhexlify('260F192E04FC4CDBE9CBDE84A0AD615F')
-    # # join a network using ABP (Activation By Personalization)
-    # lora.join(activation=LoRa.ABP, auth=(dev_addr, nwk_swkey, app_swkey))
 
     # Blue led when connected
     pycom.rgbled(0x000011)
