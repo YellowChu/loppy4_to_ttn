bf4659db903f90d8a8449386b677d9c143e20a32
YellowChu <nguyen.david1996@gmail.com>
2021-05-15 11:27:08 +0200

    moved config setting, so the downlink effect is more immediete

--- main.py
+++ main.py
@@ -34,15 +34,12 @@ lora.callback(LoRa.RX_PACKET_EVENT | LoRa.TX_PACKET_EVENT, lora_callback)
 # create socket to be used for LoRa communication
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 
-#getting config values
-rgb = pycom.nvs_get("rgb")
-dr = pycom.nvs_get("dr")
-sleep_time = pycom.nvs_get("sleep_time")
-
 # change color of led
+rgb = pycom.nvs_get("rgb")
 pycom.rgbled(rgb)
 
-# configure data rate
+# configure sf (dr0=sf12, dr1=sf11, dr2=sf10, dr3=sf9, dr4=sf8, dr5=sf7)
+dr = pycom.nvs_get("dr")
 s.setsockopt(socket.SOL_LORA, socket.SO_DR, dr)
 # make the socket blocking
 # (waits for the data to be sent and for the 2 receive windows to expire)
@@ -67,15 +64,15 @@ uplink.extend(acc_ba)
 
 # send the data
 s.send(uplink)
+# this sleep is here so the user of the module can key board interrupt
+time.sleep(2)
 # saves the lora configuration so the connection can be restored after waking up
 # from the deep sleep
 lora.nvram_save()
 # make the socket non-blocking
 # (because if there's no data received it will block forever...)
 s.setblocking(False)
-
-# this sleep is here so the user of the module can key board interrupt
-time.sleep(2)
 # machine goes to deep sleep, after it wakes up the code starts all over again
+sleep_time = pycom.nvs_get("sleep_time")
 sensors.Pysense.setup_sleep(sleep_time/1000)
 sensors.Pysense.go_to_sleep()
